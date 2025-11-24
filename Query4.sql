af310ada6363fffc0935f186542b1c9baee19db3
Rupesh chowdary Peddineni <107076022+Rupesh2728@users.noreply.github.com>
2025-11-23 21:02:36 -0500

    Delete Query4.sql

--- Query4.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.team_id, t.team_name, COUNT(*) AS players_count
-FROM Players p
-JOIN Teams t ON p.team_id = t.team_id
-GROUP BY p.team_id, t.team_name
-HAVING COUNT(*) > 10
-ORDER BY players_count DESC;
