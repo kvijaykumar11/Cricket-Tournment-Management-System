556564a014415dd07bc557401c111277e2a8e43c
Rupesh chowdary Peddineni <107076022+Rupesh2728@users.noreply.github.com>
2025-11-23 21:03:15 -0500

    Delete Query8.sql

--- Query8.sql
+++
@@ -1,7 +0,0 @@
-SELECT pt.team_id, t.team_name, pt.points
-FROM PointsTable pt
-JOIN Teams t ON pt.team_id = t.team_id
-WHERE pt.tournament_id = 1
-  AND pt.points = (
-    SELECT MAX(points) FROM PointsTable WHERE tournament_id = 1
-  );
